2726bf5035dc33454391845f8780bba01581c448
Nexus FullSync <fullsync@nexuspos.local>
2026-06-03 15:12:28 -0300

    ManualSync 3/6/2026, 03:12:28

--- list_tables.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-c = sqlite3.connect(r'F:\Nexus\nexus-pos-completo\database.db')
-rows = c.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name").fetchall()
-for r in rows:
-    print(r[0])
-c.close()
